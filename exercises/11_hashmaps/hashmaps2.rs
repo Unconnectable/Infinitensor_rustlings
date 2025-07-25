04074a2b364a2f3a10a6b058b565c70d7d6ec9b8
Unconnectable <scpr7@outlook.com>
2025-07-25 17:01:21 +0800

    finish all

--- exercises/11_hashmaps/hashmaps2.rs
+++ exercises/11_hashmaps/hashmaps2.rs
@@ -32,6 +32,8 @@ fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
         // TODO: Insert new fruits if they are not already present in the
         // basket. Note that you are not allowed to put any type of fruit that's
         // already present!
+        //scores.entry("Blue".to_string()).or_insert(50);
+        basket.entry(fruit).or_insert(2);
     }
 }
 
--- exercises/13_error_handling/errors4.rs
+++ exercises/13_error_handling/errors4.rs
@@ -10,7 +10,12 @@ struct PositiveNonzeroInteger(u64);
 impl PositiveNonzeroInteger {
     fn new(value: i64) -> Result<Self, CreationError> {
         // TODO: This function shouldn't always return an `Ok`.
-        Ok(Self(value as u64))
+        //Ok(Self(value as u64));
+        match value {
+            v if v > 0 => Ok(Self(v as u64)),
+            0 => Err(CreationError::Zero),
+            _ => Err(CreationError::Negative),
+        }
     }
 }
 
@@ -26,11 +31,11 @@ mod tests {
     fn test_creation() {
         assert_eq!(
             PositiveNonzeroInteger::new(10),
-            Ok(PositiveNonzeroInteger(10)),
+            Ok(PositiveNonzeroInteger(10))
         );
         assert_eq!(
             PositiveNonzeroInteger::new(-10),
-            Err(CreationError::Negative),
+            Err(CreationError::Negative)
         );
         assert_eq!(PositiveNonzeroInteger::new(0), Err(CreationError::Zero));
     }
